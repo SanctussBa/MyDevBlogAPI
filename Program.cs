7cae3eb11c994f1ff6950c21d5a4db419e1892db
Santa Baltmane <baltmane.santa@gmail.com>
2023-03-04 17:36:41 +0100

    Cors policy added

--- Program.cs
+++ Program.cs
@@ -5,11 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Cors Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy", builder =>
+    {
+        builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("https://localhost:7253", "http://localhost:3000");
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configuration for SQLServer connection
 builder.Services.AddDbContext<TheDevBlogDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("TheDevBlogConnectionString")));
 
@@ -23,6 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("CORSPolicy");
 
 app.UseAuthorization();
 
